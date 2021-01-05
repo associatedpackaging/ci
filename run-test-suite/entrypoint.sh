8b46f03203242aa298b64612704af9aa18f6642b
Alexey Avdeev <alexey.a.avdeev@gmail.com>
2021-01-05 14:14:36 -0600

    install yarn

--- run-test-suite/entrypoint.sh
+++ run-test-suite/entrypoint.sh
@@ -4,6 +4,10 @@
 set -e
 cd $GITHUB_WORKSPACE
 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
+
 if [ -f "Gemfile" ]; then
   echo "# Bundling..."
   gem install bundler

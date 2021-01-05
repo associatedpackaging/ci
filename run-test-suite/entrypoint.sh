e3502893503d3141ed92da1d257171115b6248fe
Alexey Avdeev <alexey.a.avdeev@gmail.com>
2021-01-05 14:36:56 -0600

    add docker compose

--- run-test-suite/entrypoint.sh
+++ run-test-suite/entrypoint.sh
@@ -7,6 +7,8 @@ cd $GITHUB_WORKSPACE
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt update && apt install -y -qq yarn
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 if [ -f "Gemfile" ]; then
   echo "# Bundling..."

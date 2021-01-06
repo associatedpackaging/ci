d9a65a8e41566c63ba50c6504361f5a5a019d0e5
Alexey Avdeev <alexx-avdeev@users.noreply.github.com>
2021-01-06 09:26:27 -0600

    Update main.js

--- test/lib/main.js
+++ test/lib/main.js
@@ -21,6 +21,11 @@ const { execSync } = require('child_process');
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
+            var command = ['bin/rails workarea:test'];
+            var testSuite = core.getInput('test');
+            if (testSuite) {
+                command.push(testSuite);
+            }
             execSync("curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -", { stdio: 'inherit' });
             execSync("echo \"deb https://dl.yarnpkg.com/debian/ stable main\" | sudo tee /etc/apt/sources.list.d/yarn.list", { stdio: 'inherit' });
             execSync("sudo apt update && sudo apt install -y -qq yarn", { stdio: 'inherit' });
@@ -29,7 +34,7 @@ function run() {
             execSync("yarn install", { stdio: 'inherit' });
             execSync("bundle exec rake workarea:services:up", { stdio: 'inherit' });
             execSync(`timeout 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9200)" != "200" ]]; do sleep 1; done' || false`, { stdio: 'inherit' });
-            execSync(core.getInput('command'), { env: Object.assign(process.env, { CI: true }), stdio: 'inherit' });
+            execSync(command.join(':'), { env: Object.assign(process.env, { CI: true }), stdio: 'inherit' });
         }
         catch (error) {
             core.setFailed(error);

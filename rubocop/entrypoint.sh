419f5dd0c592c67d6c62d241bf7d8b9f7362699c
Alexey Avdeev <alexx-avdeev@users.noreply.github.com>
2021-01-07 15:54:34 -0600

    Update entrypoint.sh

--- rubocop/entrypoint.sh
+++ rubocop/entrypoint.sh
@@ -7,6 +7,7 @@ cd $GITHUB_WORKSPACE
 if [ -f "Gemfile" ]; then
   echo "# Bundling..."
   gem install bundler
+  gem sources -a https://"${BUNDLE_GEMS__WEBLINC__COM}"@gems.weblinc.com
   #bundle install --jobs 4 --retry 3
 fi
 
@@ -15,6 +16,7 @@ if [ ! `which rubocop` ]; then
   echo "\n# Installing rubocop..."
   gem install rubocop --version "~>0.58.0"
   gem install workarea-ci --version "=3.4.14"
+  gem sources -r https://"${BUNDLE_GEMS__WEBLINC__COM}"@gems.weblinc.com
 fi
 
 if [ -z "$1" ]; then

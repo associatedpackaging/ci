1bf67aca80961999164aeecf72e2622ba8c19d10
Alexey Avdeev <alexx-avdeev@users.noreply.github.com>
2021-10-21 15:00:30 -0500

    Update entrypoint.sh

--- stylelint/entrypoint.sh
+++ stylelint/entrypoint.sh
@@ -10,7 +10,7 @@ yarn install
 # Install stylelint if needed
 if ! yarn list | grep "stylelint"; then
   echo "\n# Installing stylelint..."
-  yarn add --non-interactive --silent --dev stylelint stylelint-scss stylelint-config-recommended-scss --loglevel error
+  yarn add --non-interactive --silent --dev postcss@^8.3.3 stylelint stylelint-scss stylelint-config-recommended-scss --loglevel error
 fi
 
 if [ -z "$1" ]; then

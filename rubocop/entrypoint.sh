4ca6081a4773f1a081e912aebb418ddb0ca8cb3b
Matt Jordan <matt@mattjordan.com>
2022-05-09 09:54:47 -0500

    Fix typo

--- rubocop/entrypoint.sh
+++ rubocop/entrypoint.sh
@@ -15,7 +15,7 @@ fi
 if [ ! `which rubocop` ]; then
   echo "\n# Installing rubocop..."
   gem install rubocop --version "~>0.58.0"
-  gem install premailer -version "=1.15.0"
+  gem install premailer --version "=1.15.0"
   gem install workarea-ci --version "=3.4.14"
   gem sources -r https://"${BUNDLE_GEMS__WEBLINC__COM}"@gems.weblinc.com
 fi
